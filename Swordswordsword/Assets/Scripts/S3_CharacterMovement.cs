36a93ace7d4df0fa7ec9b4f47fe2560de6787e12
Sean Creveling <seandcreveling@gmail.com>
2015-04-03 14:47:21 -0700

    Some more movement update

---
+++ Swordswordsword/Assets/Scripts/S3_CharacterMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S3_CharacterMovement : MonoBehaviour {
+    public float MaxSpeed;
+
+    private Rigidbody2D rb2D;
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb2D.velocity = new Vector2( Input.GetAxis( "Horizontal" ), Input.GetAxis( "Vertical" ) ).normalized * MaxSpeed;
+
+        Vector3 MouseInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 PlayerToMouse = MouseInWorld - transform.position;
+        transform.eulerAngles = new Vector3( transform.eulerAngles.x, transform.eulerAngles.y, Mathf.Atan2( PlayerToMouse.y, PlayerToMouse.x ) * Mathf.Rad2Deg );
+	}
+}
---
+++ Swordswordsword/Assets/Scripts/S3_CombatStateController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S3_CombatStateController : MonoBehaviour {
+    public GameObject SwordObj;
+    public AnimationClip SwordSwingClip;
+
+    private bool slashing = false;
+    private Animator swordAnimator;
+    private float timer = 0f;
+	// Use this for initialization
+	void Start () {
+        swordAnimator = SwordObj.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if( Input.GetButton( "Attack" ) && !slashing )
+        {
+            slashing = true;
+        }
+
+        if( slashing )
+        {
+            timer += Time.deltaTime;
+            if( timer >= SwordSwingClip.length )
+            {
+                timer = 0f;
+                slashing = false;
+            }
+        }
+
+        swordAnimator.SetBool( "slashing", slashing );
+	}
+}
