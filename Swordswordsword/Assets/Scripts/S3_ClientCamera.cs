d9b567c6ab8e60037b3f36e6d4f594b187516cb4
Sean Creveling <seandcreveling@gmail.com>
2015-05-10 15:28:16 -0700

    Added Prototype map

---
+++ Swordswordsword/Assets/Scripts/S3_ClientCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S3_ClientCamera : MonoBehaviour {
+    public GameObject Player;
+
+	// Use this for initialization
+    void LateUpdate()
+    {
+        transform.position = new Vector3(
+            Player.transform.position.x,
+            Player.transform.position.y,
+            transform.position.z );
+    }
+}
