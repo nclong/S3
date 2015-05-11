b3665c85f3a5a677348d0b847c39d0dae986cec1
Sean Creveling <seandcreveling@gmail.com>
2015-05-10 18:11:40 -0700

    Started Networking Message Barrage

---
+++ Swordswordsword/Assets/Scripts/Networking/S3_GameMessage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S3_GameMessage
+{
+    float SendTime;
+    S3_GameMessageType MessageType;
+    IMessageData MessageData;
+
+    public S3_GameMessage(float time, S3_GameMessageType type, IMessageData data)
+    {
+        SendTime = time;
+        MessageType = type;
+        MessageData = data;
+    }
+
+}
---
+++ Swordswordsword/Assets/Scripts/Networking/S3_GameMessageType.cs
@@ -0,0 +1,26 @@
+﻿public enum S3_GameMessageType
+{
+    //Eric
+    ClientConnectRequest,
+    ServerConnectResponse,
+    ClientResponseAck,
+    ServerTime,
+    ClientTimeReceived,
+    ServerTimeOffset,
+    
+    //Nick
+    ServerPlayerPosDR,
+    ServerPlayerRotDR,
+    ClientPosDR,
+    ClientRotDR,
+    ServerPlayerDied,
+    ServerPlayerHit,
+    
+    //Kevin
+    ServerPlayerSwing,
+    ServerPlayerSwitch,
+    ServerStartGame,
+    ServerNewPlayer,
+    ClientSwing,
+    ClientSwitch
+}
