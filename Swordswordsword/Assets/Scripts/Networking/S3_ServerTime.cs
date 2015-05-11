b5381d61c27ca54f431e0703bc03714bc8e2d1a5
Sean Creveling <seandcreveling@gmail.com>
2015-05-10 19:43:14 -0700

    Added some prefabs and server time

---
+++ Swordswordsword/Assets/Scripts/Networking/S3_ServerTime.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S3_ServerTime : MonoBehaviour {
+
+    private static float time;
+    public static float ServerTime
+    {
+        get { return time; }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+	
+	}
+
+    public static void SetTime(float newTime)
+    {
+        time = newTime;
+    }
+
+    public static void SetOffset(float offset)
+    {
+        time += offset;
+    }
+}
