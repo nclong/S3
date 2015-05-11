9c86c1b60f0d1ea7b609cd9534d297149121a957
KTZimmerle <kzimmerl@uci.edu>
2015-05-10 18:34:10 -0700

    Kevin's MessageDataTypes Defined so far

---
+++ Swordswordsword/Assets/Scripts/Networking/MessageDataTypes/ServerNewPlayerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class ServerNewPlayerData
+{
+    //player stuff required
+    //velocity
+    float [] velocity;
+    //position
+    float []  position;
+    //rotation
+    float [] angle;
+    //sword equiped (assumed to have only one)
+    //health and health bar
+    int health;
+}
+
---
+++ Swordswordsword/Assets/Scripts/Networking/MessageDataTypes/ServerPlayerSwitchData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class ServerPlayerSwitchData
+{
+    float animateSpeed;
+    //first check if player has a 2nd weapon on
+    bool givesPermission;
+}
