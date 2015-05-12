20ba074c468ca9f915caec0568f315d3f31557fd
Nick Long <nclong@uci.edu>
2015-05-12 12:40:16 -0700

    Added a new scene

---
+++ Swordswordsword/Assets/Scripts/S3_RoleSelection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class S3_RoleSelection : MonoBehaviour {
+    public InputField HostIp;
+
+
+	public void SelectJoin()
+    {
+        string HostIpString = HostIp.text;
+    }
+
+    public void SelectHost()
+    {
+        Application.LoadLevel( "tussle_server" );
+    }
+}
