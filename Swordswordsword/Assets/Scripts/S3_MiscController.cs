3a1ba491410b32da4fe92a35b9f20fdd96da6687
KTZimmerle <kzimmerl@uci.edu>
2015-05-26 23:20:12 -0700

    Tab button now toggles scoreboard visibility
    Only changes alpha. If it needs to be deactivated by player, it will
    need to be refactored.

---
+++ Swordswordsword/Assets/Scripts/S3_MiscController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S3_MiscController : MonoBehaviour {
+
+    public CanvasGroup ScoreBoard;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ScoreBoard.alpha = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (ScoreBoard.alpha == 1.0f)
+                ScoreBoard.alpha = 0.0f;
+            else
+                ScoreBoard.alpha = 1.0f;
+        }
+	}
+}
