c84a7dc4b0814bb7ec2785808e34036fe46711d8
Vincent Wing <vwing@uci.edu>
2015-06-02 21:14:53 -0700

    Removed sword speed animation

--- Swordswordsword/Assets/Scripts/S3_CombatStateController.cs
+++ Swordswordsword/Assets/Scripts/S3_CombatStateController.cs
@@ -6,9 +6,7 @@ public class S3_CombatStateController : MonoBehaviour {
     public AnimationClip SwordSwingClip;
     public GameObject RightShoulder;
 
-    private bool slashing = false;
     private Animator swordAnimator;
-    private float timer = 0f;
     private S3_CharacterMovement characterMovement;
     private S3_SwordManager swordManager;
 	// Use this for initialization
@@ -30,26 +28,7 @@ public class S3_CombatStateController : MonoBehaviour {
     public void SwingSword(float offset)
     {
         AnimatorStateInfo info = swordAnimator.GetCurrentAnimatorStateInfo( 0 );
-        float ServerTimeToFinish = SwordSwingClip.length;
-        float localTimeToFinish = ServerTimeToFinish + offset;
-        float localSpeed = localTimeToFinish / SwordSwingClip.length;
-        swordAnimator.speed = localSpeed;
         swordAnimator.SetBool("slashing", true);
-
-        S3_GunSword gun = SwordObj.GetComponent<S3_GunSword>();
-        if( gun != null && gun.enabled )
-        {
-            gun.FireBullet();
-        }
-
-        S3_SwordZooka zooka = SwordObj.GetComponent<S3_SwordZooka>();
-        if( zooka != null && zooka.enabled )
-        {
-            zooka.fireRocket();
-        }
-
-        Debug.Log( "Slash Start" );
-        Debug.Log( info.IsName( "ShoulderSlash" ) );
     }
 
     public AnimatorStateInfo GetAnimatorInfo(int x)
