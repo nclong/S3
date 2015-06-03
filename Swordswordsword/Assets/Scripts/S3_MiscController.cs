aa7cb26a3987f8aa01ee0e3b34ddb7c1c9ac320c
KTZimmerle <kzimmerl@uci.edu>
2015-06-02 23:53:16 -0700

    Client can now bring up scoreboard (untested)

--- Swordswordsword/Assets/Scripts/S3_MiscController.cs
+++ Swordswordsword/Assets/Scripts/S3_MiscController.cs
@@ -7,14 +7,23 @@ public class S3_MiscController : MonoBehaviour {
 	public Text[] Names;
 	public Text[] Scores;
 	public Text[] Lag;
-	private S3_ServerPlayerManager playerManager;
+    private S3_ServerPlayerManager playerManager;
+    private S3_ClientPlayerManager cPlayerManager;
 
 	private Text[] EmptyStrings;
 
 	// Use this for initialization
 	void Start ()
     {
-		playerManager = GameObject.Find ("ServerPlayerManager").GetComponent<S3_ServerPlayerManager> ();
+        if (GameObject.Find("ServerPlayerManager"))
+        {
+            playerManager = GameObject.Find("ServerPlayerManager").GetComponent<S3_ServerPlayerManager>();
+        }
+        else
+        {
+            playerManager = null;
+            cPlayerManager = GameObject.Find("ClientPlayerManager").GetComponent<S3_ClientPlayerManager>();
+        }
         ScoreBoard.alpha = 0.0f;
 		for(int i = 0; i < 4; ++i)
 		{
@@ -27,24 +36,48 @@ public class S3_MiscController : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if (ScoreBoard.alpha == 1.0f)
-			{
-                ScoreBoard.alpha = 0.0f;
-				for(int i = 0; i < 4; ++i)
-				{
-					Names[i].text = Scores[i].text = Lag[i].text = string.Empty;
-				}
-			}
+            if (playerManager != null)
+            {
+                if (ScoreBoard.alpha == 1.0f)
+                {
+                    ScoreBoard.alpha = 0.0f;
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        Names[i].text = Scores[i].text = Lag[i].text = string.Empty;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < playerManager.CurrentPlayers; ++i)
+                    {
+                        Names[i].text = playerManager.PlayerNames[i];
+                        Scores[i].text = playerManager.GetComponent<S3_GameManager>().PlayerScores[i].ToString();
+                        Lag[i].text = playerManager.GetPing(i).ToString();
+                    }
+                    ScoreBoard.alpha = 1.0f;
+                }
+            }
             else
-			{
-				for(int i = 0; i < playerManager.CurrentPlayers; ++i)
-				{
-					Names[i].text = playerManager.PlayerNames[i];
-					Scores[i].text = playerManager.GetComponent<S3_GameManager>().PlayerScores[i].ToString();
-					Lag[i].text = playerManager.GetPing(i).ToString();
-				}
-                ScoreBoard.alpha = 1.0f;
-			}
+            {
+                if (ScoreBoard.alpha == 1.0f)
+                {
+                    ScoreBoard.alpha = 0.0f;
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        Names[i].text = Scores[i].text = Lag[i].text = string.Empty;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < cPlayerManager.CurrentPlayers; ++i)
+                    {
+                        Names[i].text = cPlayerManager.PlayerNames[i];
+                        Scores[i].text = cPlayerManager.GetComponent<S3_GameManager>().PlayerScores[i].ToString();
+                        Lag[i].text = cPlayerManager.GetPing(i).ToString();
+                    }
+                    ScoreBoard.alpha = 1.0f;
+                }
+            }
         }
 	}
 }
